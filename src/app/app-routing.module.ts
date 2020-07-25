132c5f297bd063273faa77cbc445a3468ef9b415
Const <konospas@gmail.com>
2020-07-25 18:30:57 +0300

    add welcome module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-
-const routes: Routes = [{
-  path: 'registration',
-  loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule)
-}];
+const routes: Routes = [
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then(
+      m => m.RegistrationModule),
+  },
+  { path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then(
+      m => m.WelcomeModule),
+  }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
